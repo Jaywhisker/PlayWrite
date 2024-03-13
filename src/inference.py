99050591717ae535a858d04f03ff5ee76556c74d
Jaywhisker <chengweixuan7@gmail.com>
2024-03-14 01:17:59 +0800

    Added notebooks for finetuning CNNs and training with attention, bug fixes for train and eval

--- src/inference.py
+++ src/inference.py
@@ -20,9 +20,11 @@ def unnormalize(image:np.array, mean:np.array, std:np.array):
     return image
 
 
-def caption_image(model, dataloader, vocabulary:Vocabulary, device:str, mean:np.array=np.array([0.485, 0.456, 0.406]), std:np.array=np.array([0.229, 0.224, 0.225]), num_batches:int=1, num_images:int=5, max_length:int=50, show_plot:bool=False):
+def caption_image(model, dataloader, vocabulary:Vocabulary, device:str, mean:np.array, std:np.array, num_batches:int=1, num_images:int=5, max_length:int=50, show_plot:bool=False):
     """
     Function to generate model predictions from a dataloader
+
+    Note: model class should already contain function caption image with 4 inputs (image, vocabularly, device, max_length) and 2 outputs generation caption, attention (Or None)
 
     Arg:
         model: model to general model prediction (ensure that your model has the function caption_image)
@@ -52,7 +54,7 @@ def caption_image(model, dataloader, vocabulary:Vocabulary, device:str, mean:np.
             image = features[i].unsqueeze(0).to(device)
             
             #generate captions from model
-            generated_caption = model.caption_image(image, vocabulary, max_length=max_length)
+            generated_caption, attention = model.caption_image(image, vocabulary, device, max_length=max_length)
             
             #plot image and captions
             if show_plot:
@@ -72,6 +74,67 @@ def caption_image(model, dataloader, vocabulary:Vocabulary, device:str, mean:np.
 
     return all_predictions
 
+
+
+def multiple_model_captions(model_list, dataloader, vocabulary:Vocabulary, device:str, mean:np.array=np.array([0.485, 0.456, 0.406]), std:np.array=np.array([0.229, 0.224, 0.225]), num_batches:int=1, num_images:int=5, max_length:int=50, show_plot:bool=False):
+    """
+    Function to generate model predictions for multiple models from the same dataloader
+
+    Arg:
+        model_list: list of model to general model prediction (ensure that your model has the function caption_image)
+        dataloader: dataset to generate prediction
+        vocabulary (Vocabulary): dataset vocabulary
+        device (str): cpu or cuda,
+        mean (np.array): Numpy array of the mean used for normalisation
+        std (np.array): Numpy array of the std used for normalisation
+        num_batches (int, optional): how many batches iterating from dataloader, defaults to 1
+        num_image (int, optional): how many images per batch to generate model prediction, defaults to 5
+        max_length (int, optional): maximum length of generated captions, defaults to 50
+        show_plot (bool, optional): show the image and generated captions in a plot, defaults to False
+    
+    Returns:
+        all_predictions (dict): Dictionary containing the dictionary of all generated captions for each model and list of actual captions
+    
+    """
+    for model in model_list:
+        model.eval()
+
+    #dictionary containing all the generated predictions and actual predictions
+    all_predictions = {'Predicted': {}, 'Possible Captions': []}
+
+    #iterate for num of batches we are testing
+    for j in range(num_batches):
+        #load images from dataloader
+        features, annotations, all_annotations = next(iter(dataloader))
+        
+        #take first k from batch
+        for i in range(num_images):
+            image = features[i].unsqueeze(0).to(device)
+            all_captions = []
+
+            for idx, model in enumerate(model_list):
+                #generate captions from model
+                generated_caption = model.caption_image(image, vocabulary, max_length=max_length)
+                all_captions.append(" ".join(generated_caption[1:-1]))
+                model_predictions = all_predictions['Predicted'].get(f"model_{idx}", [])
+                all_predictions['Predicted'][f"model_{idx}"] = model_predictions.append(all_captions)
+
+            #plot image and captions
+            if show_plot:
+                fig, ax = plt.subplots(figsize=(5, 5))
+                img = features[i].squeeze()
+                img = unnormalize(img, mean, std)  # Unnormalize the image
+                img = np.transpose(img.numpy(), (1, 2, 0))
+                ax.imshow(img)
+                ax.axis('off')
+                ax.set_title(f'All Model Predictions:\n{"\n".join(all_captions)}\n\nAll Possible Predictions:\n' + "\n".join(all_annotations[i]), loc='left')
+
+                plt.tight_layout()
+                plt.show()
+
+            all_predictions['Possible Captions'].append(all_annotations[i])
+
+    return all_predictions
 
 
 if __name__ == "__main__":
